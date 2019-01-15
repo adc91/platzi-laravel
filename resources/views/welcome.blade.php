b3ae796b847d4cbe47fae6f0167b5c53a0bda370
Angel Camacho <adc.3191@gmail.com>
2019-01-15 18:10:56 -0300

    Validation and Data Creation in Laravel with Eloquent

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,26 @@
 </div>
 
 <div class="row">
+    <form action="/messages/create" method="post">
+        <div class="form-group @if ($errors->has('message')) is-invalid @endif">
+            <input type="text" name="message" class="form-control @if ($errors->has('message')) is-invalid @endif" placeholder="Que estás pensando?">
+            @if ($errors->has('message'))
+                @foreach ($errors->get('message') as $error)
+                    <div class="invalid-feedback">{{ $error }}</div>
+                @endforeach
+            @endif
+        </div>
+        @csrf
+    </form>
+</div>
+
+<div class="row">
   @forelse ($messages as $message)
     <div class="col-6">
-      <img src="{{ $message['image'] }}" class="img-thumbnail">
+      <img src="{{ $message->image }}" class="img-thumbnail">
       <p class="card-text">
-        {{ $message['content'] }}
-        <a href="/messages/{{ $message['id'] }}">Leer más</a>
+        {{ $message->content }}
+        <a href="/messages/{{ $message->id }}">Leer más</a>
       </p>
     </div>
   @empty
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@
 */
 
 Route::get('/', 'PagesController@home');
+Route::get('/messages/{message}', 'MessagesController@show');
+
+Route::post('/messages/create', 'MessagesController@create');
